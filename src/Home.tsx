287e26716f0b31fbd39e5be66229ba171cf8de27
acharmedmatrix <acharmedmatrix@gmail.com>
2021-11-10 18:15:07 -0500

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -173,7 +173,7 @@ const Home = (props: HomeProps) => {
 
       {wallet && <p>Your Wallet Balance: {(balance || 0).toLocaleString()} SOL</p>}
 
-      {wallet && <p>Tokens Available at 0.4 SOL: {itemsAvailable}{goLiveDate}</p>}
+      {wallet && <p>Tokens Available at 0.4 SOL: {itemsAvailable}</p>}
 
       {wallet && <p>Tokens Redeemed: {itemsRedeemed}</p>}
 
